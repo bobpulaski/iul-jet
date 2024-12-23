ee2c475b11286ea279c97e43070df88ca018b501
bobpulaski <82018695+bobpulaski@users.noreply.github.com>
2024-12-23 13:16:58 +0300

    23-12-2024--job-fileupload

--- app/Livewire/IulFormComponent.php
+++ app/Livewire/IulFormComponent.php
@@ -3,13 +3,42 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use Livewire\Attributes\Validate;
 
 class IulFormComponent extends Component
 {
+    use WithFileUploads;
+
+    #[Validate('file|max:81920')] // 80MB Max
+    public $inputFile;
+
+
+
 
     public function start()
     {
-        $this->dispatch(event: 'create-iul-started');
+        // $this->dispatch(event: 'create-iul-started');
+
+        // Обработка загрузки файла, если он был загружен
+        if ($this->inputFile) {
+            $this->uploadFile();
+        }
+    }
+
+    public function uploadFile()
+    {
+        $fileName = $this->inputFile->getClientOriginalName(); // Получение имени файла
+        $this->inputFile->storeAs(path: 'uploads', name: $fileName); // Сохранение файла в папку 'uploads'
+
+        // Находим путь к загруженному файлу
+
+        $filePath = storage_path('app/private/uploads/' . $fileName);
+
+        // Получаем CRC32B хэш загруженного файла
+        $hash = hash_file('crc32b', $filePath);
+
+        dd($hash); // Вывод хеша для проверки
     }
 
 
