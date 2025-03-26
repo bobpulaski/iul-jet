9f66effed5e48824a04449f8356eb9860356aa4e
Bob Pulaski Home <mynameisempty@yandedex.ru>
2025-03-26 20:23:22 +0300

    Spatie Honeypot added --home <x-honeypot />

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,7 @@
 
         <form method="POST" action="{{ route('register') }}">
             @csrf
-            @honeypot
+            <x-honeypot />
 
             <div>
                 <x-label for="name" value="{{ __('Имя') }}" />
