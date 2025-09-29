53877943f459ea28118064bc925942e5cd76892d
bobpulaski <82018695+bobpulaski@users.noreply.github.com>
2025-09-29 13:22:12 +0300

    add a post

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -1,6 +1,14 @@
 <x-article-layout :post="$post">
+
+    @php
+        $date = new DateTime();
+        $date->setTimestamp($post['meta']['published']);
+        $published = $date->format('d.m.Y');
+    @endphp
+
     <main>
         <div id="container">
+            <small style="color: #999;">{!!  $published !!}</small>
             {!! $post['body'] !!}
         </div>
     </main>
--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,11 +1,26 @@
 <x-blog-layout :posts="$posts">
-<main>
+    <main>
         <div id="container">
             <h1>Блог</h1>
             @foreach($posts as $post)
-                <h2 class=""><a href="{{ route('article', ['slug' => $post['slug']]) }}">{!!  $post['meta']['title'] !!}</a></h2>
-                <p>{!!  $post['meta']['description'] !!}</p>
-                <a href="{{ route('article', ['slug' => $post['slug']]) }}" class="button button-outline">Читать далее</a>
+                <div
+                    style="box-shadow: 0px 0px 0px 1px #DCDCDC; padding: 2rem; border-radius: 8px; margin-bottom: 3rem">
+                    <h2><a href="{{ route('article', ['slug' => $post['slug']]) }}">{!!  $post['meta']['title'] !!}</a>
+                    </h2>
+                    <p>{!!  $post['meta']['description'] !!}</p>
+                    <div style="display: flex; justify-content: space-between;">
+
+                        @php
+                            $date = new DateTime();
+                            $date->setTimestamp($post['meta']['published']);
+                            $published = $date->format('d.m.Y');
+                        @endphp
+
+                        <small style="color: #999">{!!  $published !!}</small>
+                        <a href="{{ route('article', ['slug' => $post['slug']]) }}" class="button button-outline">Читать
+                            далее</a>
+                    </div>
+                </div>
         @endforeach
     </main>
 </x-blog-layout>
